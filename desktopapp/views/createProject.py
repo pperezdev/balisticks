e02e0c2044fa493e5fc1e7d57b374dada72da53e
Quentin <quentinprs62@gmail.com>
2021-07-23 16:14:40 +0200

    add forms to create tash and Project

--- desktopapp/views/createProject.py
+++ desktopapp/views/createProject.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog as fd
 from .summary import init_summary
+
 
 class CreateProjectPage(tk.Frame):
 
@@ -16,31 +18,61 @@ class CreateProjectPage(tk.Frame):
         self.tagstype = []
         self.datetype = []
         try:
-            self.tagstype.extend(["A","B","C"])
+            self.tagstype.extend(["A", "B", "C"])
             self.datetype.extend(["A", "B", "C"])
-            #setup API
+            # setup API
             # Get Tag Type
             # Get Date type
         except:
-            ttk.showerror.showwarning("API", "Error 200")
+            tk.showerror.showwarning("API", "Error 200")
 
     def create_widget(self):
         label = ttk.Label(self, text=f"Creer votre projet", font=self.controller.title_font).grid(row=0,
-                                                                                                      column=2,
-                                                                                                      sticky='w')
+                                                                                                  column=2,
+                                                                                                  sticky='w')
         organisation_name_label = ttk.Label(self, text="Nom")
-        organisation_name_label.grid(row=3, column=2, sticky='w')
+        organisation_name_label.grid(row=2, column=2, sticky='w')
 
         organisation_name_entry = tk.Entry(self)
-        organisation_name_entry.grid(row=4, column=2, sticky='w')
+        organisation_name_entry.grid(row=3, column=2, sticky='w')
+
+        variableOrganisation = tk.StringVar()
+        organisation = ["Organisation 1", "Organisation 2", "Organisation 3", "Organisation 4"]
+        variableOrganisation.set(organisation[0])
+
+        # --------------------------------------------------
+        #               Create Label
+        # --------------------------------------------------
+
+        ttk.Label(self, text="Description").grid(row=4, column=2, sticky='w')
+        ttk.Label(self, text="Libelle").grid(row=6, column=2, sticky='w')
+        ttk.Label(self, text="Organisations").grid(row=8, column=2, sticky='w')
+        ttk.Label(self, text="User").grid(row=10, column=2, sticky='w')
+
+        # --------------------------------------------------
+        #               Create Entry
+        # --------------------------------------------------
+
+        description = tk.Entry(self)
+        libelle = tk.Entry(self)
+        entryOrganisation = tk.OptionMenu(self, variableOrganisation, *organisation)
+        user = tk.Entry(self)
+
+        # --------------------------------------------------
+        #               Placement of objects
+        # --------------------------------------------------
+
+        description.grid(row=5, column=2)
+        libelle.grid(row=7, column=2)
+        entryOrganisation.grid(row=9,column=2)
+        user.grid(row=11, column=2)
 
         ttk.Button(self, text="create",
-                   command=lambda: push(self)).grid(row=5, column=2, sticky='w')
+                   command=lambda: push(self)).grid(row=12, column=2, sticky='w')
 
         init_summary(self, ttk)
 
-        self.entry_values.extend([organisation_name_entry])
-
+        self.entry_values.extend([organisation_name_entry, description, libelle])
 
     def validation(self):
         """
@@ -61,7 +93,8 @@ class CreateProjectPage(tk.Frame):
 
         return True, "Votre projet est créer"
 
+
 def push(self):
     m = self.controller.mother(self)
-    if(m):
+    if (m):
         self.controller.show_frame("ProjectPage")
--- desktopapp/views/createTask.py
+++ desktopapp/views/createTask.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from .summary import init_summary
+from tkcalendar import *
+
 
 class CreateTaskPage(tk.Frame):
 
@@ -16,9 +18,9 @@ class CreateTaskPage(tk.Frame):
         self.tagstype = []
         self.datetype = []
         try:
-            self.tagstype.extend(["A","B","C"])
+            self.tagstype.extend(["A", "B", "C"])
             self.datetype.extend(["A", "B", "C"])
-            #setup API
+            # setup API
             # Get Tag Type
             # Get Date type
         except:
@@ -26,21 +28,59 @@ class CreateTaskPage(tk.Frame):
 
     def create_widget(self):
         label = ttk.Label(self, text=f"Creer votre STICKS", font=self.controller.title_font).grid(row=0,
-                                                                                                      column=2,
-                                                                                                      sticky='w')
+                                                                                                  column=2,
+                                                                                                  sticky='w')
         organisation_name_label = ttk.Label(self, text="Nom")
-        organisation_name_label.grid(row=3, column=2, sticky='w')
+        organisation_name_label.grid(row=2, column=2, sticky='w')
 
         organisation_name_entry = tk.Entry(self)
-        organisation_name_entry.grid(row=4, column=2, sticky='w')
+        organisation_name_entry.grid(row=3, column=2, sticky='w')
+
+        variableTask = tk.StringVar()
+        task_priority = ["Option 1", "Option 2", "Option 3", "Option 4"]
+        variableTask.set(task_priority[0])
+
+        # --------------------------------------------------
+        #               Create Label
+        # --------------------------------------------------
+
+        ttk.Label(self, text="Libelle").grid(row=4, column=2, sticky='w')
+        ttk.Label(self, text="Date Debut").grid(row=6, column=2, sticky='w')
+        ttk.Label(self, text="Date fin").grid(row=8, column=2, sticky='w')
+        #ttk.Label(self, text="Collaborate").grid(row=10, column=2, sticky='w')
+        ttk.Label(self, text="Priorité de la task").grid(row=13, column=2, sticky='w')
+
+        # --------------------------------------------------
+        #               Create Entry
+        # --------------------------------------------------
+
+        libelle = tk.Entry(self)
+        dateDebut = Calendar(self, selectmode = 'day',
+               year = 2021, month = 7,
+               day = 22, width=15)
+        dateFin = Calendar(self, selectmode = 'day',
+               year = 2021, month = 7,
+               day = 22, width=15)
+        #collaborate = tk.Entry(self)
+        entryTaskPriotite = tk.OptionMenu(self, variableTask, *task_priority)
+
+        # --------------------------------------------------
+        #               Placement of objects
+        # --------------------------------------------------
+
+        libelle.grid(row=5, column=2)
+        dateDebut.grid(row=7, column=2, pady=10)
+        dateFin.grid(row=9, column=2, pady=10)
+        #collaborate.grid(row=11, column=2)
+        entryTaskPriotite.grid(row=13, column=2)
 
         ttk.Button(self, text="create",
-                   command=lambda: push(self)).grid(row=5, column=2, sticky='w')
+                   command=lambda: push(self)).grid(row=20, column=2, sticky='w')
 
         init_summary(self, ttk)
 
-        self.entry_values.extend([organisation_name_entry])
-
+        self.entry_values.extend([organisation_name_entry, libelle, dateFin,
+                                  dateFin, variableTask])
 
     def validation(self):
         """
@@ -54,6 +94,7 @@ class CreateTaskPage(tk.Frame):
             if not entrie.get():  # si vide
                 return False, "entry is empty"
             tab.append(entrie.get())
+            #tab.append(entrie)
 
         print("Je suis le tab", tab)
 
@@ -61,7 +102,8 @@ class CreateTaskPage(tk.Frame):
 
         return True, "Votre sticks est créer"
 
+
 def push(self):
     m = self.controller.mother(self)
-    if(m):
+    if (m):
         self.controller.show_frame("TaskPage")
